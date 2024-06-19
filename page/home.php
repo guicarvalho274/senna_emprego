47afde410101a54d9bb4fc9891b114302055b9f2
Guilherme Santos Carvalho <guicarvalho274@gmail.com>
2024-06-19 01:33:25 -0300

    atualização do conteúdo e estilo do site

--- page/home.php
+++ page/home.php
@@ -35,3 +35,93 @@
         </div><!--label-interna-->
     </div><!--container-->
 </section><!--sessao_two-->
+
+<section class="sessao_three">
+    <div class="container">
+        <h1>Por que <span>trabalhar conosco:</span></h1>
+        <div class="flex">
+            <div class="box">
+                <div>
+                    <div class="img">
+                        <img src="<?php echo INCLUDE_PATH?>imagens/img-icon-box-1.svg" alt="">
+                    </div><!--box-->
+                    <h3>Altos Ganhos:</h3>
+                    <p>Você pode ganhar até R$4.000,00 por mês, além de benefícios atrativos.</p>
+                </div>
+            </div><!--box-->
+            <div class="box">
+                <div>
+                    <div class="img">
+                        <img src="<?php echo INCLUDE_PATH?>imagens/img-icon-box-2.svg" alt="">
+                    </div><!--box-->
+                    <h3>Crescimento Profissional:</h3>
+                    <p>Acreditamos no seu potencial e oferecemos oportunidades reais de crescimento dentro da empresa.</p>
+                </div>
+            </div><!--box-->
+            <div class="box">
+                <div>
+                    <div class="img">
+                        <img src="<?php echo INCLUDE_PATH?>imagens/img-icon-box-3.svg" alt="">
+                    </div><!--box-->
+                    <h3>Benefícios:</h3>
+                    <p>Oferecemos plano de saúde, vale academia, e vale transporte ou gasolina para quem tem veículo próprio.</p>
+                </div>
+            </div><!--box-->
+            <div class="box">
+                <div>
+                    <div class="img">
+                        <img src="<?php echo INCLUDE_PATH?>imagens/img-icon-box-4.svg" alt="">
+                    </div><!--box-->
+                    <h3>Ambiente de Trabalho:</h3>
+                    <p>Valorizamos um ambiente de trabalho saudável e motivador, onde você poderá desenvolver suas habilidades e se destacar.</p>
+                </div>
+            </div><!--box-->
+        </div><!--flex-->
+
+        <div class="btn-action" id="action" data-direcao="form">
+            <p>Quero a Vaga</p>
+        </div><!--btn-action-->
+    </div><!--container-->
+</section><!--sessao_three-->
+
+<section class="sesssao_four">
+    <div class="container">
+        <h1>O que oferecemos:</h1>
+        <ul class="list">
+            <li class="flex">
+                <img src="<?php echo INCLUDE_PATH?>imagens/checked-icon.svg" alt="">
+                <p>Salário fixo de R$1.550,00 na CLT (carteira assinada).</p>
+            </li>
+            <li class="flex">
+                <img src="<?php echo INCLUDE_PATH?>imagens/checked-icon.svg" alt="">
+                <p>Comissão por metas atingidas, podendo alcançar até R$4.000,00 mensais.</p>
+            </li>
+            <li class="flex">
+                <img src="<?php echo INCLUDE_PATH?>imagens/checked-icon.svg" alt="">
+                <p>Comissão por metas atingidas, podendo alcançar até R$4.000,00 mensais.</p>
+            </li>
+            <li class="flex">
+                <img src="<?php echo INCLUDE_PATH?>imagens/checked-icon.svg" alt="">
+                <p>Plano de saúde HAPVIDA para você e seus dependentes.</p>
+            </li>
+            <li class="flex">
+                <img src="<?php echo INCLUDE_PATH?>imagens/checked-icon.svg" alt="">
+                <p>Plano odontológico.</p>
+            </li>
+            <li class="flex">
+                <img src="<?php echo INCLUDE_PATH?>imagens/checked-icon.svg" alt="">
+                <p>Vale transporte ou gasolina.</p>
+            </li>
+            <li class="flex">
+                <img src="<?php echo INCLUDE_PATH?>imagens/checked-icon.svg" alt="">
+                <p>Vale academia.</p>
+            </li>
+            <li class="flex">
+                <img src="<?php echo INCLUDE_PATH?>imagens/checked-icon.svg" alt="">
+                <p>Vale alimentação.</p>
+            </li>
+        </ul><!--list-->
+
+        <h1>Ganhos podendo alcancar mais de <span>R$4.000,00</span></h1>
+    </div><!--container-->
+</section><!--sessao_four-->
