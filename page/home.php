2815a866ede9b927c33762ee35278b31fa349b2c
Guilherme Santos Carvalho <guicarvalho274@gmail.com>
2024-06-18 22:09:47 -0300

    Atualização na landing page: style.css

--- page/home.php
+++ page/home.php
@@ -18,3 +18,20 @@
         </div><!--flex-->
     </div><!--container-->
 </section><!--sessao_one-->
+
+<section class="sessao_two">
+    <div class="container">
+        <div class="label-interna">
+            <div class="flex">
+                <div class="img-box"></div>
+                <div class="texto">
+                    <h1>Sobre nós</h1>
+                    <p>A Senna Automóveis é uma rede de lojas de veículos presente em diversos estados, dedicada a realizar o sonho de nossos clientes, oferecendo carros de alta qualidade com as melhores condições de pagamento possíveis. Nosso compromisso é não apenas com os clientes, mas também com nossos colaboradores, proporcionando um ambiente de trabalho excepcional.</p>
+                    <div class="btn-action" id="action" data-direcao="form">
+                        <p>Quero a Vaga</p>
+                    </div><!--btn-action-->
+                </div><!--texto-->
+            </div><!--flex-->
+        </div><!--label-interna-->
+    </div><!--container-->
+</section><!--sessao_two-->
